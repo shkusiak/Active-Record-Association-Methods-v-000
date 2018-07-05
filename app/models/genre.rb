c0b47ed201fbb4fb2a68be5c4c250e6b07ee5492
Shea Kusiak <shkusiak@gmail.com>
2018-07-05 23:27:32 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,8 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists.distinct.pluck(:name)
+    self.artists.collect do |a|
+      a.name
+    end
   end
 end
